a6762ed97c7ac92e51542d3fbf560fb8a401b710
ashish952208 <70108437+ashish952208@users.noreply.github.com>
2020-08-23 23:20:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyDixSjnjNLCsHbAC6NICx_e0alW65kzSAE",
+  authDomain: "ashishlinks-53840.firebaseapp.com",
+  databaseURL: "https://ashishlinks-53840.firebaseio.com",
+  projectId: "ashishlinks-53840",
+  storageBucket: "ashishlinks-53840.appspot.com",
+  messagingSenderId: "801172158836",
+  appId: "1:801172158836:web:ee259c9b5971f685445421",
+  measurementId: "G-FLHN1K9BRZ"
 };
 
      
